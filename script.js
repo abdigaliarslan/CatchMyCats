c80b78bfae0d933e15f10bca5d7f2a3dcd40cddf
abdigaliarslan <abdigali.a20@gmail.com>
2025-08-04 15:38:23 +0500

    add-lives

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ if (!document.referrer.includes("Start/start.html")) {
         }
 
         let score = 0;
+        let lives = 3;
+        let intervalId;
         const images = [
             "source/cat1.png",
             "source/cat2.png",
@@ -11,16 +13,19 @@ if (!document.referrer.includes("Start/start.html")) {
             "source/cat5.png",
         ];
         const image = document.getElementById('image');
+        const livesDisplay = document.getElementById('lives');
+        const gameOver = document.getElementById('game-over');
+        
 
 
         function RandomCoordinates(){
             const imgW = image.offsetWidth || 150;
             const imgH = image.offsetHeight || 150;
-            const maxX = window.innerWidth - imgW;
+            const maxX = window.innerWidth - imgW;  
             const maxY = window.innerHeight - imgH;
             
             const x = Math.floor(Math.random() * maxX);
-            const y = Math.floor(Math.random() * maxY);
+            const y = Math.floor(Math.random() * maxY); 
             
             return {x, y};
         }
@@ -34,20 +39,62 @@ if (!document.referrer.includes("Start/start.html")) {
                 image.style.top = `${y}px`;
         }
 
-        function handleClick(){
+        function updateLives(){
+            livesDisplay.textContent = "❤️".repeat(lives);
+            if (lives==0){
+                alert("Game Over");
+                location.reload();
+            }
+        }
+
+        function handleClick(e){
+            e.stopPropagation();    
             score++;
             document.getElementById('score').textContent = score;
             RandomCat();
         }
+
+        image.addEventListener('click', handleClick);
+
+        function updateLives() {
+            livesDisplay.innerText = "❤️".repeat(Math.max(0, lives));
+            livesDisplay.style.color = "red";
+            livesDisplay.style.transition = "0.2s";
+            if (lives == 0) {
+                GameOver()
+            }};
+            
+
+
+        document.addEventListener('click', function (e) {
+            if (e.target !== image) {
+                lives--;
+                updateLives();
+            }
+        });
         
+        function GameOver(){
+            console.log(">>> GameOver CALLED");
+            document.getElementById('game-over').classList.remove('hidden');
+            clearInterval(intervalId)
+            if(image) image.remove();
+        }
+
+        function restartGame(){
+            location.reload();
+        }
 
         const maxSpeed = localStorage.getItem('maxSpeed');
+        const info = document.getElementById('info');
         if (!maxSpeed){
-            document.getElementById('info').textContent = 'Please set your max speed';
+            info.textContent = 'Please set your max speed';
         }
         else{
-            document.getElementById('info').textContent = `Your max speed is ${maxSpeed} seconds`;
+            info.textContent = `Your max speed is ${maxSpeed} seconds`;
         }
-         
-        window.onload = RandomCat;
-        setInterval(RandomCat, maxSpeed * 1000);
+
+        
+        window.onload = () => {
+            RandomCat();
+            intervalId = setInterval(RandomCat, 1000 * maxSpeed);
+        }
